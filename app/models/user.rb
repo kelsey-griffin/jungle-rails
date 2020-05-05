1874937427d2ff86b01171fe4466d617109fac87
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-04 21:08:42 -0400

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates_presence_of :name, :email, :password, :password_confirmation
+  validates :email , uniqueness: { case_sensitive: false }
+  validates :password, length: { in: 6..20 }
+  validates :password_confirmation, length: { in: 6..20 }
+
+
+ 
 end
