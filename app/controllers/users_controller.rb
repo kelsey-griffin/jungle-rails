507e708b82bc92bb68afedbce7b530005224a8a2
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-04 21:05:43 -0400

    trim whitespace and downcase sign up form inputs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    
+    # Clean sign up form inputs
+    user.name = user.name.strip.titlecase.to_s
+    user.email = user.email.downcase.strip.to_s
+      
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
