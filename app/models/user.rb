9e5148c4e736e5f5d2f9e50b4c410d3c3cbac6f9
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-04 21:13:19 -0400

    write auth-with-cred method, account for padding and case

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,19 @@ class User < ActiveRecord::Base
   validates :password, length: { in: 6..20 }
   validates :password_confirmation, length: { in: 6..20 }
 
+  def self.authenticate_with_credentials(email, password)
 
+    # Clean user input
+    email_clean = email.downcase.strip
+    
+    user = User.find_by_email(email_clean)
+    
+    if user && user.authenticate(password)
+      @user = user
+    else 
+      nil
+    end
+
+  end
  
 end
